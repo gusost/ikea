1775d2e548701d4cdf4d4d3a087af34054b28a61
Gustav Öst <gustav.ost@snowsoftware.com>
2022-02-20 20:55:21 +0100

    Added blind control

---
+++ blind.go
@@ -0,0 +1,27 @@
+package ikea
+
+import (
+	"fmt"
+)
+
+func SetBlindPosition(deviceId int, position float32) error {
+	device, err := GetDevice(deviceId)
+
+	if err != nil {
+		return fmt.Errorf("error getting device state: %+v", err)
+	}
+
+	if device.Type != 7 {
+		return fmt.Errorf("device is not a blind: %+v", err)
+	}
+
+	if device.BlindControl[0].Position != position {
+
+		result, err := client.PutDevicePositioning(deviceId, position)
+		if err != nil {
+			return fmt.Errorf("error setting blind: %+v", err)
+		}
+		fmt.Printf("%v - %v\n", device.Name, result.Msg)
+	}
+	return nil
+}
